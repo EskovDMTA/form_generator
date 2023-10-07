e58de6becf0289e0da49472cd1bf5a380e5c6006
eskovdmt <DmtEskov@gmail.com>
2023-10-07 19:31:15 +0300

    feat: add form field generation

--- lib/hexlet/hexlet-code/tags.rb
+++ lib/hexlet/hexlet-code/tags.rb
@@ -3,17 +3,44 @@
 require_relative "paired_tags"
 # autoload("HexletCode", "lib/hexlet/hexlet-code/hexlet_code")
 module HexletCode
-  def self.form_for(_user, attributes = {})
+  def self.form_for(user, attributes = {})
     attributes[:action] = attributes.delete(:url) if attributes.key?(:url)
-    base_form_attr = { action: "#", method: "post" }
-    HexletCode::Tag.build("form", base_form_attr.merge(attributes))
+    form_attributes = { action: "#", method: "post" }.merge(attributes)
+    form_body = block_given? ? yield(FormBodyBuilder.new(user)) : ""
+    HexletCode::Tag.build("form", form_attributes, form_body)
+  end
+
+  class FormBodyBuilder
+    attr_reader :user
+
+    def initialize(user)
+      @user = user
+      @result = ""
+    end
+
+    def input(name, attributes = {})
+      value = user.public_send(name)
+      attributes[:value] = value if value
+
+      if attributes[:as] == :text
+        text_attributes = { name: name, cols: 20, rows: 40 }.merge(attributes).reject do |k, _v|
+          %i[as value].include?(k)
+        end
+        @result += Tag.build("textarea", text_attributes, attributes[:value])
+      else
+        input_attributes = { name: name, type: "text", value: value }.merge(attributes)
+        @result += Tag.build("input", input_attributes)
+      end
+      @result
+    end
   end
 
   class Tag
     class << self
-      def build(tag_name, attributes = {})
+      def build(tag_name, attributes = {}, content = "")
         tag_body = yield if block_given?
-        "<#{tag_name}#{parsing_attributes(attributes)}>#{tag_body || ""}#{paired_tag?(tag_name)}"
+        content = "\n#{content}" if tag_name == "form"
+        "<#{tag_name}#{parsing_attributes(attributes)}>#{tag_body || content || ""}#{paired_tag?(tag_name)}\n"
       end
 
       def parsing_attributes(attributes = {})
--- test/hexlet/hexlet-code/test_tags.rb
+++ test/hexlet/hexlet-code/test_tags.rb
@@ -4,35 +4,38 @@ require "test_helper"
 require_relative "../../../lib/hexlet/hexlet-code/tags"
 
 class TestTag < Minitest::Test
-  def test_return_form_without_action
-    assert_equal "<form action=\"#\" method=\"post\"></form>", HexletCode.form_for("name")
-  end
-
-  def test_return_form_with_url
-    assert_equal "<form action=\"/user\" method=\"post\"></form>", HexletCode.form_for("name", url: "/user")
+  def test_form_for
+    expected_form = File.read("/Users/eskovdmt/Documents/projects/hexlet/hexlet-form/rails-project-63/test/hexlet/hexlet-code/fixtures/form.html")
+    struct_user = Struct.new(:name, :job, :gender, keyword_init: true)
+    user = struct_user.new name: "rob", job: "developer", gender: "Male"
+    form = HexletCode.form_for user do |f|
+      f.input :name, class: "hexlet"
+      f.input :job, as: :text
+    end
+    assert_equal expected_form, form
   end
 
   def test_single_quotes_tag
-    assert_equal "<br>", HexletCode::Tag.build("br")
+    assert_equal "<br>", HexletCode::Tag.build("br").chomp
   end
 
   def test_double_quotes_tag
-    assert_equal "<div></div>", HexletCode::Tag.build("div")
+    assert_equal "<div></div>", HexletCode::Tag.build("div").chomp
   end
 
   def test_tag_with_tezt
-    assert_equal "<div>Tag with text</div>", HexletCode::Tag.build("div") { "Tag with text" }
+    assert_equal "<div>Tag with text</div>", HexletCode::Tag.build("div") { "Tag with text" }.chomp
   end
 
   def test_tag_with_attributes
     assert_equal "<div data=\"id-123-qwe\" class=\"hexlet-tag\"></div>",
                  HexletCode::Tag.build("div", data: "id-123-qwe",
-                                              class: "hexlet-tag")
+                                              class: "hexlet-tag").chomp
   end
 
   def test_tag_with_attributes_and_text
     assert_equal "<div data=\"id-123-qwe\" class=\"hexlet-tag\">With text</div>",
                  HexletCode::Tag.build("div", data: "id-123-qwe",
-                                              class: "hexlet-tag") { "With text" }
+                                              class: "hexlet-tag") { "With text" }.chomp
   end
 end
