98e9013dd9e60401f4117490748db3d9c3c05106
eskovdmt <DmtEskov@gmail.com>
2023-10-15 18:33:45 +0300

    fix: remove form print

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -11,11 +11,3 @@ module HexletCode
     FormRender.render_html(builded_form.form_attr, builded_form.form_body)
   end
 end
-
-struct_user = Struct.new(:name, :job, :gender, keyword_init: true)
-user = struct_user.new name: 'rob', job: 'developer', gender: 'Male'
-HexletCode.form_for user do |f|
-  f.input :name, class: 'hexlet'
-  f.input :job, as: :text
-  f.submit 'Wow'
-end
--- lib/hexlet_code/form_render.rb
+++ lib/hexlet_code/form_render.rb
@@ -5,11 +5,11 @@ module HexletCode
     class << self
       def render_html(form_attr, form_body)
         form = "<form action=\"#{form_attr[:action]}\" method=\"#{form_attr[:method]}\">\n"
-        print(form_attr)
         form_body.each do |tag|
           form += HexletCode::Tag.build(tag[:tag], tag.except(:tag))
         end
         form += '</form>'
+        form
       end
     end
   end
