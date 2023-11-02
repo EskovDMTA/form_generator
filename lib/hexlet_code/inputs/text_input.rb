f733a6068abc55b5f9fe4eb03381c51fac86edba
eskovdmt <DmtEskov@gmail.com>
2023-11-02 19:52:47 +0300

    feat: remove destructive operations

--- lib/hexlet_code/inputs/text_input.rb
+++ lib/hexlet_code/inputs/text_input.rb
@@ -8,9 +8,9 @@ module HexletCode
 
       def input
         common_attributes = { tag: 'textarea', cols: COLS, rows: ROWS }
-        @input[:content] = @input.delete(:value)
-        @input.delete(:type)
-        input_attributes = @input.merge(common_attributes)
+        @input[:content] = @input[:value]
+        attributes = @input.except(:type, :value)
+        input_attributes = attributes.merge(common_attributes)
         HexletCode::Tag.build(input_attributes[:tag], input_attributes.except(:tag))
       end
     end
